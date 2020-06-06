7823b7eee9f2267fe6b8ff9fcab65260139e4689
sun <760172827@qq.com>
2020-06-06 22:20:16 +0800

    typescripe环境搭建

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+  entry: {
+    app: path.join(__dirname, "./src/index.tsx")
+  },
+  output: {
+      filename: "[name].[hash].js",
+      path: path.join(__dirname, "./dist")
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  // externals: {
+  //     "react": "React",
+  //     "react-dom": "ReactDOM"
+  // },
+  devServer: {
+    host: '0.0.0.0',
+    compress: true,
+    port: '8080',
+    contentBase: path.join(__dirname, './dist'),
+    hot: true,
+    overlay: {
+      errors: true
+    },
+    publicPath: '/',
+    historyApiFallback: {
+      index: './index.html'
+    },
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, './index.html')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
